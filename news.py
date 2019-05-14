c457fa534e578eb9b601e8b010e9693bdcd351f5
Alexander Wade <alexanderwade4@gmail.com>
2019-05-14 09:42:20 -0400

    Add html skeleton of html wrap

---
+++ news.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+HTML_WRAP =
+'''\
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+        </title>
+        <style>
+            h1, form { text-align: center; }
+        </style>
+    </head>
+    <body>
+        <h1>News Data Query</h1>
+        <form method=post>
+            <div><textarea id="query" name="query"></textarea></div>
+        </form>
+    </body>
+</html>
+'''
+
+def funcname(self, parameter_list):
+    pass
+
+def funcname(self, parameter_list):
+    pass
+
+if __name__ == "__main__":
+    pass
