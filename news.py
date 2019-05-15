646820361ad963e958ae73845ea2349ef178f89f
Alexander Wade <alexanderwade4@gmail.com>
2019-05-15 08:14:30 -0400

    Add initial database connections

--- news.py
+++ news.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python3
+#code for the logs analysis project
 
 import psycopg2
 
 DBNAME = "news"
 
-def mostPopularArticles(self, parameter_list):
-    
+def mostPopularArticles():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("")
+    articles = c.fetchall()
+    db.close()
+    return articles
 
-def mostPopularAuthors(self, parameter_list):
-    pass
+def mostPopularAuthors():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute()
+    authors = c.fetchall()
+    db.close()
+    return authors
 
-def requestErrors(self, parameter_list):
-    pass
+
+def requestErrors():
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute()
+    errors = c.fetchall()
+    db.close()
+    return errors
+
 
 if __name__ == "__main__":
     mostPopularArticles()
