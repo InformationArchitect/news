1338b5a539bf46c3b04c5eb302d397404ec2eb90
Alexander Wade <alexanderwade4@gmail.com>
2019-05-17 09:20:41 -0400

    Add print formatting in sql and python

--- news.py
+++ news.py
@@ -52,7 +52,7 @@ def requestErrors():
     db = psycopg2.connect(dbname=DATABASE)
     c = db.cursor()
     c.execute('''
-                SELECT errors.day, 100*cast(errors.count AS FLOAT)/successes.count AS "Error_Rate" FROM (
+                SELECT SUBSTRING(CAST(errors.day AS TEXT), 1, 10), 100*CAST(errors.count AS FLOAT)/successes.count AS "Error_Rate" FROM (
                         SELECT day,count(*) FROM (select date_trunc('day', time) AS "day", SUBSTRING (status, 1, 3) FROM log WHERE status
                         != '200 OK' ORDER BY time ASC) AS "errors" GROUP BY day ORDER BY day ASC
                     ) AS "errors", 
@@ -60,7 +60,7 @@ def requestErrors():
                         SELECT day,count(*) FROM (select date_trunc('day', time) AS "day", SUBSTRING (status, 1, 3) FROM log ORDER BY time ASC)
                         AS "successes" GROUP BY day ORDER BY day ASC
                     ) AS "successes"
-                    WHERE 100*cast(errors.count AS FLOAT)/successes.count > 1;
+                    WHERE 100*CAST(errors.count AS FLOAT)/successes.count > 1;
     ''')
     errors = c.fetchall()
     db.close()
@@ -68,9 +68,19 @@ def requestErrors():
 
 
 if __name__ == "__main__":
-    print(mostPopularArticles())
-    print(mostPopularAuthors())
-    print(requestErrors())
-
+    print("Top 3 Articles by Views")
+    articles = list(mostPopularArticles())
+    for article in articles:
+        print('"{}" -- {} views'.format(article[0], article[1]))
+    print("\n")
+    print("Authors Listed by Views")
+    authors = list(mostPopularAuthors())
+    for author in authors:
+        print('{} -- {} views'.format(author[0], author[1]))
+    print("\n")
+    print("Days Where Error Rates Surpassed 1 Percent")
+    errors = list(requestErrors())
+    for error in errors:
+        print('{0} -- {1:.1f}% errors'.format(error[0], error[1]))
 
     
