0b0cea5c0d1fec7459a4d7a7c2debdc23b0ff697
vagrant <vagrant@vagrant.vm>
2019-05-21 21:40:25 +0000

    Bugfix: Average error rates

--- news.py
+++ news.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-#code for the logs analysis project
+# code for the logs analysis project
 
 import psycopg2
 
 DATABASE = "news"
+
 
 def mostPopularArticles():
     db = psycopg2.connect(dbname=DATABASE)
     c = db.cursor()
     c.execute('''
                 SELECT articles.title, mostpopular.numviews FROM (
-                    SELECT 
+                    SELECT
                         SUBSTRING (path, 10) AS "slug", count(*) AS "numviews"
-                        FROM log WHERE path != '/' AND status = '200 OK' GROUP BY SUBSTRING (path, 10)
+                        FROM log WHERE path != '/' AND
+                            status = '200 OK' GROUP BY SUBSTRING (path, 10)
                         ORDER BY count(*) DESC LIMIT 3
                     ) AS mostpopular, articles
                     WHERE mostpopular.slug = articles.slug
@@ -22,21 +24,28 @@ def mostPopularArticles():
     db.close()
     return articles
 
+
 def mostPopularAuthors():
     db = psycopg2.connect(dbname=DATABASE)
     c = db.cursor()
     c.execute('''
-                SELECT popular.name, SUM(popular.numviews) AS views FROM (
-                    SELECT authors.name, idpopular.slug, idpopular.numviews FROM (
-                        SELECT articles.author, articles.slug, mostpopular.numviews FROM (
-                            SELECT 
-                                SUBSTRING (path, 10) AS "slug", count(*) AS "numviews"
-                                FROM log WHERE path != '/' AND status = '200 OK' GROUP BY SUBSTRING (path, 10)
+                SELECT popular.name, SUM(popular.numviews)
+                AS views FROM (
+                    SELECT authors.name, idpopular.slug,
+                    idpopular.numviews FROM (
+                        SELECT articles.author, articles.slug,
+                        mostpopular.numviews FROM (
+                            SELECT
+                                SUBSTRING (path, 10) AS "slug",
+                                count(*) AS "numviews"
+                                FROM log WHERE path != '/' AND
+                                status = '200 OK' GROUP BY
+                                SUBSTRING (path, 10)
                                 ORDER BY count(*) DESC
                             ) AS mostpopular, articles
                             WHERE mostpopular.slug = articles.slug
                             ORDER BY mostpopular.numviews DESC
-                        ) AS idpopular, authors 
+                        ) AS idpopular, authors
                         WHERE idpopular.author = authors.id
                         ORDER BY numviews
                     ) AS popular
@@ -52,15 +61,25 @@ def requestErrors():
     db = psycopg2.connect(dbname=DATABASE)
     c = db.cursor()
     c.execute('''
-                SELECT SUBSTRING(CAST(errors.day AS TEXT), 1, 10), 100*CAST(errors.count AS FLOAT)/successes.count AS "Error_Rate" FROM (
-                        SELECT day,count(*) FROM (select date_trunc('day', time) AS "day", SUBSTRING (status, 1, 3) FROM log WHERE status
-                        != '200 OK' ORDER BY time ASC) AS "errors" GROUP BY day ORDER BY day ASC
-                    ) AS "errors", 
+                SELECT TO_CHAR(errors.day, 'FMMonth DD, YYYY') as "date",
+                    AVG(100*CAST(errors.count AS FLOAT)/total.count)
+                    AS "Error_Rate" FROM (
+                        SELECT day,count(*) FROM
+                            (select CAST(time AS DATE) AS "day",
+                            SUBSTRING (status, 1, 3) FROM log WHERE status
+                            != '200 OK' ORDER BY time ASC)
+                        AS "errors" GROUP BY day ORDER BY day ASC
+                    ) AS "errors",
                     (
-                        SELECT day,count(*) FROM (select date_trunc('day', time) AS "day", SUBSTRING (status, 1, 3) FROM log ORDER BY time ASC)
-                        AS "successes" GROUP BY day ORDER BY day ASC
-                    ) AS "successes"
-                    WHERE 100*CAST(errors.count AS FLOAT)/successes.count > 1;
+                        SELECT day,count(*) FROM
+                            (select CAST(time AS DATE)
+                            AS "day", SUBSTRING (status, 1, 3) FROM log
+                            ORDER BY time ASC)
+                        AS "total" GROUP BY day ORDER BY day ASC
+                    ) AS "total"
+                    WHERE 100*CAST(errors.count AS FLOAT)/total.count > 1
+                    GROUP BY date
+                    ORDER BY date ASC;
     ''')
     errors = c.fetchall()
     db.close()
@@ -82,5 +101,3 @@ if __name__ == "__main__":
     errors = list(requestErrors())
     for error in errors:
         print('{0} -- {1:.1f}% errors'.format(error[0], error[1]))
-
-    
